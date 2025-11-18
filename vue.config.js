ac7a00ddb23f49c0a4d475d190841cde1d2cbcc4
Kaido Kossas <kaido.kossas@gmail.com>
2025-11-18 21:48:22 +0200

    Redo config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-const path = require('path')  // ← required import
+const path = require('path')
 
 module.exports = defineConfig({
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   transpileDependencies: true,
 
   configureWebpack: {

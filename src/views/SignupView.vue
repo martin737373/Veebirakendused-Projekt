54648cdff21830e209edf8883b847a8ce9391eb6
Martin <93202736+martin737373@users.noreply.github.com>
2025-11-21 21:50:21 +0200

    Add SignupView.vue

---
+++ src/views/SignupView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="signup">
+    <SignupComponent />
+  </div>
+</template>
+
+<script>
+import SignupComponent from "@/components/SignupComponent.vue";
+
+export default {
+  name: "SignupView",
+  components: {
+    SignupComponent,
+  },
+};
+</script>
+
+<style scoped>
+.signup {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  min-height: 0;
+}
+</style>
